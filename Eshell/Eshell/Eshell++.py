57de98ea653ad2ce1fcbea006798d3c836004a31
Eoin_Irwin <C13439802@mydit.ie>
2016-10-19 22:09:20 +0100

    Added in an ip address function and login screen function

--- Eshell/Eshell/Eshell++.py
+++ Eshell/Eshell/Eshell++.py
@@ -1,6 +1,30 @@
-import getpass, subprocess
+import getpass, subprocess, time, ipgetter
 
-systemCommands = {'pw': 'pwd', 'ifc': 'ifconfig eth0', 'ls': 'ls'}
+systemCommands = {'pw': 'pwd', 'ifc': 'ifconfig eth0', 'ls': 'ls', 'ui': 'uid'}
+
+
+def get_ip_address():
+    ip = ipgetter.myip()
+    print ip
+
+
+def initial_login():
+    print 64 * '='
+    print """\nWelcome to Eshell++ 1.0.0 LTS (GNU/Linux 1.1.0-1-generic x86_64)
+
+         * Documentation:  https://help.Eshell++.com
+         * Management:     https://landscape.canonical.com
+         * Support:        https://Eshell++.com/advantage
+
+        15 packages can be updated.
+        4 updates are security updates.
+
+
+        Last login: """ + time.strftime("%a %b %d %H:%M:%S %Y") + ' from', get_ip_address()
+    print 64 * '='
+
+
+initial_login()
 
 
 def user_input(choice):
@@ -10,10 +34,10 @@ def user_input(choice):
 
 
 def main():
-    subprocess.call('', shell=True)
+    subprocess.call('''''', shell=True)
 
     while True:
-        x = raw_input(getpass.getuser() + "@eshell++:#")
+        x = raw_input(getpass.getuser() + "@Eshell++:#")
         x = user_input(x)
 
         subprocess.call(x, shell=True)
