1fa6c07ddd3c46db95b19b99b54f5ca06c923243
Eoin_Irwin <C13439802@mydit.ie>
2016-10-22 14:18:14 +0100

    Added in docstrings to document functions

--- Eshell/Eshell/Eshell++.py
+++ Eshell/Eshell/Eshell++.py
@@ -5,11 +5,13 @@ systemCommands = {'pw': 'pwd', 'ifc': 'ifc', 'dt': 'date', 'ls': 'ls', 'cmatrix'
 
 
 def get_ip_address():
+    """Get the ip address of current system"""
     ip = ipgetter.myip()
     print ip
 
 
 def initial_login():
+    """Initial login screen you are met with"""
     print 64 * '='
     print """\nWelcome to Eshell++ 1.0.0 LTS (GNU/Linux 1.1.0-1-generic x86_64)
 
@@ -26,6 +28,7 @@ def initial_login():
 
 
 def ifc(choices):
+    """Ifconfig which by default displays eth0, unless a tail is supplied"""
     if len(x) > 1:
         os.system('ifconfig {0}'.format(x[1]))
     else:
@@ -33,6 +36,7 @@ def ifc(choices):
 
 
 def ls(choices):
+    """ls command which will only display ls unless a tail like ls -l is entered"""
     if len(x) > 1:
         os.system('ls {0}'.format(x[1]))
     else:
@@ -40,32 +44,39 @@ def ls(choices):
 
 
 def pwd(choices):
+    """Basic print the working directory"""
     os.system('pwd')
 
 
 def date(choices):
+    """Print date command in the following format"""
     print time.strftime("%Y%m%d%H%M%S")
 
 
 def pacman(choices):
+    """Entering this cmd runs the game"""
     os.system('pacman4console')
 
 
 def nsnake(choices):
+    """Entering this cmd runs the game"""
     os.system('nsnake')
 
 
 def enter(choices):
+    """Entering this, enters the matrix"""
     os.system('cmatrix')
 
 
 def help_cmd(choices):
+    """Display all of the options available to the user"""
     print 'Limited commands available to: ', getpass.getuser()
     for k, v in systemCommands.iteritems():
         print k
 
 
 def user_input(choice):
+    """evaluate the users choice and return the command, if sudo is entered print a message, exit if it is entered"""
     for x, y in systemCommands.iteritems():
         if choice[0] == x:
             eval(y + '({0})'.format(choice))
@@ -81,6 +92,7 @@ def user_input(choice):
     return False
 
 
+"""Keep the programming running while x is not=exit"""
 x = ''
 initial_login()
 while x != 'exit':
