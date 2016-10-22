0f119b3a0c35963010e354ff768e9bad0bde4911
Eoin_Irwin <C13439802@mydit.ie>
2016-10-22 15:12:54 +0100

    Updated docstring + final ud function

--- Eshell/Eshell/Eshell++.py
+++ Eshell/Eshell/Eshell++.py
@@ -1,17 +1,25 @@
-import getpass, time, ipgetter, os, sys
+import getpass, time, ipgetter, os, sys, subprocess
 
 systemCommands = {'pw': 'pwd', 'ifc': 'ifc', 'dt': 'date', 'ls': 'ls', 'cmatrix': 'enter',
-                  'pacman4console': 'pacman', 'nsnake': 'nsnake', 'help': 'help_cmd'}
+                  'pacman4console': 'pacman', 'nsnake': 'nsnake', 'help': 'help_cmd', 'ud': 'ud'}
 
 
 def get_ip_address():
-    """Get the ip address of current system"""
+    """
+     Get the ip address of current system
+     :param: None
+     :return: None
+    """
     ip = ipgetter.myip()
     print ip
 
 
 def initial_login():
-    """Initial login screen you are met with"""
+    """
+     Initial login screen you are met with
+     :param choices: None
+     :return: None
+   """
     print 64 * '='
     print """\nWelcome to Eshell++ 1.0.0 LTS (GNU/Linux 1.1.0-1-generic x86_64)
 
@@ -28,7 +36,11 @@ def initial_login():
 
 
 def ifc(choices):
-    """Ifconfig which by default displays eth0, unless a tail is supplied"""
+    """
+      Ifconfig which by default displays eth0, unless a tail is supplied
+      :param choices: None
+      :return: None
+    """
     if len(x) > 1:
         os.system('ifconfig {0}'.format(x[1]))
     else:
@@ -36,7 +48,11 @@ def ifc(choices):
 
 
 def ls(choices):
-    """ls command which will only display ls unless a tail like ls -l is entered"""
+    """
+      ls command which will only display ls unless a tail like ls -l is entered
+      :param choices: None
+      :return: None
+    """
     if len(x) > 1:
         os.system('ls {0}'.format(x[1]))
     else:
@@ -44,39 +60,67 @@ def ls(choices):
 
 
 def pwd(choices):
-    """Basic print the working directory"""
+    """
+      Print working directory
+      :param choices: None
+      :return: None
+    """
     os.system('pwd')
 
 
 def date(choices):
-    """Print date command in the following format"""
+    """
+      Print date command in the following format
+      :param choices: None
+      :return: None
+    """
     print time.strftime("%Y%m%d%H%M%S")
 
 
 def pacman(choices):
-    """Entering this cmd runs the game"""
+    """
+      Enter the cmd to run the game
+      :param choices: None
+      :return: None
+     """
     os.system('pacman4console')
 
 
 def nsnake(choices):
-    """Entering this cmd runs the game"""
+    """
+       Play a classic
+      :param choices: None
+      :return: None
+     """
     os.system('nsnake')
 
 
 def enter(choices):
-    """Entering this, enters the matrix"""
+    """
+      Enter the matrix
+      :param choices: None
+      :return: None
+     """
     os.system('cmatrix')
 
 
 def help_cmd(choices):
-    """Display all of the options available to the user"""
+    """
+      Display all of the options available to the user
+      :param choices: None
+      :return: True: Evaluate statement the user entered
+     """
     print 'Limited commands available to: ', getpass.getuser()
     for k, v in systemCommands.iteritems():
         print k
 
 
 def user_input(choice):
-    """evaluate the users choice and return the command, if sudo is entered print a message, exit if it is entered"""
+    """
+       evaluate the users choice and return the command, if sudo is entered print a message, exit if it is entered
+       :param choice: None
+       :return: True: Evaluate statement the user entered
+    """
     for x, y in systemCommands.iteritems():
         if choice[0] == x:
             eval(y + '({0})'.format(choice))
@@ -92,7 +136,25 @@ def user_input(choice):
     return False
 
 
-"""Keep the programming running while x is not=exit"""
+def ud(choices):
+    """
+    Prints userid, groupid, username, main groupname, iNode of users home directory.
+    :param choices: None
+    :return: None
+    """
+    uname = subprocess.check_output(['whoami'])
+    uname = uname.strip("\r\n")
+    uid = subprocess.check_output(['id', '-u', '{0}'.format(uname)])
+    uid = uid.strip("\r\n")
+    gid = subprocess.check_output(['id', '-g', '{0}'.format(uname)])
+    gid = gid.strip("\r\n")
+    grpmain = subprocess.check_output(['groups', '{0}'.format(uname)])
+    grpmain = grpmain.split(' ')
+    grpmain = grpmain[2]
+    inode = subprocess.check_output(['ls', '-id'])
+    print("{0},{1},{2},{3},{4}".format(uid, gid, uname, grpmain, inode))
+
+
 x = ''
 initial_login()
 while x != 'exit':
